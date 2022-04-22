453dc4448bf71df81d04eb91e1907cba4cdf1344
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-04-22 08:37:30 -0400

    Time: 374 ms (27.03%), Space: 15.5 MB (24.07%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -5,16 +5,22 @@ class Solution:
         left = 0
         right = len(nums)-1
         
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            elif (nums[mid] < target):
-                # cut off bottom half
-                left = mid + 1 
-            else:
-                # cut off top half
-                right = mid - 1
+        return self.helper(left, right, nums, target)
+    
+    def helper(self, left:int, right:int, nums: List[int], target: int) -> int:
+        
+        if left>right:
+            return -1
+        
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif (nums[mid] < target):
+            # cut off bottom half
+            return self.helper(mid+1, right, nums, target)
+        else:
+            # cut off top half
+            return self.helper(left, mid-1, nums, target)
                  
-        # it isn't there
+        # it isn't there    
         return -1
