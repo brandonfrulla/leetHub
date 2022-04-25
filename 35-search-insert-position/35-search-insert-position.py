a1ec36b95114d07c15f289ee42ae80cc8c5b7411
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-04-25 12:43:50 -0400

    Time: 70 ms (44.80%), Space: 14.8 MB (42.96%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,24 +1,21 @@
 class Solution:
-    def helper(self, _nums: List[int], _target: int, _l: int, _h: int) -> int:
-        while _l <= _h:
-            _m = (_l + _h) // 2
-                
-            if _nums[_m] == _target:
-                return _m
-            elif _target > _nums[_m]:
-                _l = _m + 1
-            else:
-                _h = _m - 1
-                
-        return _l
     
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, h = 0, len(nums)-1
         
-        if target < nums[l]:
-            return 0
-        elif target > nums[h]:
-            return len(nums)
-        else:
-            return self.helper(nums, target, l, h)
+        if target in nums:
+            return nums.index(target)
+        
+        l = 0
+        h = len(nums)
+        
+        while l < h:
+            
+            mid = (l + h) // 2
+            
+            if target > nums[mid]: 
+                l = mid + 1
+            else:
+                h = mid
+                
+        return l
             
