c4478603631d44a94af8c0f8caef961c483d330c
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-04-25 11:52:40 -0400

    Time: 1231 ms (62.76%), Space: 25 MB (40.74%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        #must start on "day 1", can't get any cheaper than day 1 to start
         minPurchase = prices[0]
         maxProfit = 0
         
         for price in prices:
             curProfit = price - minPurchase
+            #save new profit, if more is made on this trade than previous high
             if curProfit > maxProfit:
                 maxProfit = curProfit
+            #adjust buy-in to new lower price, when one is found
             if price < minPurchase:
                 minPurchase = price
                 
