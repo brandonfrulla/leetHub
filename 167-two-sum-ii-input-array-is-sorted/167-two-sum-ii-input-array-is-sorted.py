1335c38b9d72104a921df0f72b00bb181de58681
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-06 19:05:28 -0400

    Time: 204 ms (32.30%), Space: 14.9 MB (90.32%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        trash = []
+        
+        for i in range(len(numbers)):
+            if numbers[i] not in trash:
+                for j in range(i+1, len(numbers)):
+                    if numbers[i] + numbers[j] == target:
+                        return [i+1,j+1]
+                
+            #nums[i] is not in the solution
+            trash.append(numbers[i])
+            
