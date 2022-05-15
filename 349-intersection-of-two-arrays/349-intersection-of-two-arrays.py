83ecb4e2d4782d8029d7236441ad5f6a743954bd
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-15 14:12:17 -0400

    Time: 66 ms (51.67%), Space: 14.2 MB (25.25%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,16 +1,12 @@
 class Solution:
+    def s_intersection(self, s1, s2) -> List[int]:
+        return [n for n in s1 if n in s2]
+    
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         s1 = set(nums1)
         s2 = set(nums2)
         
-        cross = []
         if (len(s1) > len(s2)):
-            for n in s2:
-                if n in s1 and n not in cross:
-                    cross.append(n)
+            return self.s_intersection(s2, s1)
         else:
-            for n in s1:
-                if n in s2 and n not in cross:
-                    cross.append(n)
-                    
-        return cross
+            return self.s_intersection(s1, s2)
