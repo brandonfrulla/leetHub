65d2ec93122ed5f87dec0acc76fe3d183d76100f
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-15 14:50:47 -0400

    Time: 158 ms (23.66%), Space: 14.3 MB (44.91%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -2,9 +2,13 @@ class Solution:
     
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
         
+        #remove duplicates
         s1,s2,s3 = set(arr1), set(arr2), set(arr3)
+        
+        #get the xing of s1, with the xing of s2, with the xing of s1 and s3
         x = s1.intersection(s2.intersection(s3.intersection(s1)))
         
+        #convert it to an array so it can be sorted
         l= []
         for _x in x:
             l.append(_x)
