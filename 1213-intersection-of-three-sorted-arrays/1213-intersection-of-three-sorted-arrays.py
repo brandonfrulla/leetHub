178228485d040c19184bfac766813b3b0d009fb1
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-15 14:49:33 -0400

    Time: 83 ms (94.13%), Space: 14.3 MB (44.91%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -1,19 +1,14 @@
 class Solution:
-    def xing(self, s1, s2) -> List[int]:
-        return [x for x in s1 if x in s2]
     
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        s1 = set(arr1)
-        s2 = set(arr2)
-        s3 = set(arr3)
         
-        x1 = self.xing(s1, s2)
-        x2 = self.xing(s1, s3)
-        x3 = self.xing(s2, s3)
+        s1,s2,s3 = set(arr1), set(arr2), set(arr3)
+        x = s1.intersection(s2.intersection(s3.intersection(s1)))
         
-        x4 = self.xing(x1, x3)
-        x5 = self.xing(x2, x3)
+        l= []
+        for _x in x:
+            l.append(_x)
+            
+        l.sort()
         
-        x5.sort()
-        
-        return x5
+        return l
