6e8e82bf0bc5683353a644d7baa293d67016d172
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-19 16:48:13 -0400

    Time: 266 ms (38.36%), Space: 15.5 MB (97.29%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,12 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l2 = [num for num in nums if num != 0]
+        #get all non-zeros
+        l = [num for num in nums if num != 0]
         
         i = 0
         while i < len(nums):
-            if i < len(l2):
-                nums[i] = l2[i]
+            #push non-zeros to front
+            if i < len(l):
+                nums[i] = l[i]
+            #everything else is 0
             else:
                 nums[i] = 0
             i += 1
