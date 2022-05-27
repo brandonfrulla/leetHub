d7890c002178651bbd67f437c621093c97fb00b1
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-26 21:50:06 -0400

    Time: 240 ms (28.25%), Space: 17.1 MB (10.91%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        trash = []
-        for n in nums:
-            if n not in trash:
-                if nums.count(n) == 1:
-                    return n
-                else: 
-                    trash.append(n)
+        return 2 * sum(set(nums)) - sum(nums)
