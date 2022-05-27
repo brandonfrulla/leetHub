bdb9c1413a9f62e68282cee4edac62327200f06a
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-27 19:34:45 -0400

    Time: 736 ms (46.82%), Space: 14.3 MB (71.16%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.py
@@ -0,0 +1,24 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        perim = 0
+        
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if grid[r][c] == 1:
+                    perim += 4
+                    
+                    #look above
+                    if (r > 0 and grid[r-1][c] == 1):
+                        perim -= 1
+                    #look left
+                    if (c > 0 and grid [r][c-1] == 1):
+                        perim -= 1
+                    #look down
+                    if (r+1 < len(grid) and grid [r+1][c] == 1):
+                        perim -= 1
+                    #look right
+                    if (c+1 < len(grid[0]) and grid [r][c+1] == 1):
+                        perim -= 1
+                        
+        return perim
