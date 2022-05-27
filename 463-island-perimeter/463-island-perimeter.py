7467a08504b30961ae0717fc6ed39e9efcd16269
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-27 19:36:48 -0400

    Time: 984 ms (18.27%), Space: 14.1 MB (93.74%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -2,6 +2,7 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
         perim = 0
+        w, l = len(grid), len(grid[0])
         
         for r, row in enumerate(grid):
             for c, val in enumerate(row):
@@ -15,10 +16,10 @@ class Solution:
                     if (c > 0 and grid [r][c-1] == 1):
                         perim -= 1
                     #look down
-                    if (r+1 < len(grid) and grid [r+1][c] == 1):
+                    if (r+1 < w and grid [r+1][c] == 1):
                         perim -= 1
                     #look right
-                    if (c+1 < len(grid[0]) and grid [r][c+1] == 1):
+                    if (c+1 < l and grid [r][c+1] == 1):
                         perim -= 1
                         
         return perim
