bc055d3efcc215474cb853633b67e8fdd7e2c6ed
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2022-05-30 11:11:51 -0400

    Time: 65 ms (5.26%), Space: 14 MB (11.99%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,11 +2,8 @@ class Solution:
     options = {0:0, 1:1, 2:2, 3:3}
     
     def climbStairs(self, n: int) -> int:
-        return self.helper(n)
-        
-    def helper(self, n: int) -> int:
         if n in self.options:
             return self.options[n]
         else: 
-            self.options[n] = self.helper(n-1) + self.helper(n-2)
+            self.options[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
             return self.options[n]
